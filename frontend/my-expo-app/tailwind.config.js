ddc1413d454ed1e638f967c45b4bbf50a0ab81bd
Parash12345 <166775122+Parash12345@users.noreply.github.com>
2025-12-07 08:27:01 +0545

    chore: add frontend project configuration and dependencies

---
+++ frontend/my-expo-app/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './App.{js,ts,tsx}',
+    './components/**/*.{js,ts,tsx}',
+    './screens/**/*.{js,ts,tsx}',
+    './navigation/**/*.{js,ts,tsx}',
+    './contexts/**/*.{js,ts,tsx}',
+  ],
+
+  presets: [require('nativewind/preset')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
